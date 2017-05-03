39105be3c768f6653a8642fa4cb5717158aa445f
naveedaheer <naveedaheer@gmail.com>
2017-05-03 18:20:24 +0500

    children routes added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,12 +6,18 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { AuthGuard } from './auth.service';
 import { SignupComponent } from './signup/signup.component';
+import { AddTodoComponent } from './add-todo/add-todo.component';
+import { ViewTodoComponent } from './view-todo/view-todo.component';
 
 export const router: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
     { path: 'signup', component: SignupComponent },
-    { path: 'home', component: HomeComponent, canActivate: [AuthGuard] }
+    { path: 'home', children:[
+        {path: '',  component: HomeComponent, canActivate: [AuthGuard], pathMatch: 'full'  },
+        {path: 'add-todo', component: AddTodoComponent },
+        {path: 'view-todo', component: ViewTodoComponent }
+    ]}
 
 ]
 
